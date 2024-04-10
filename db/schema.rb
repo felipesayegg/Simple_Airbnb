15d8f6bb7194f6157888c006bd119d01c3bed2cf
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-04-10 14:56:49 -0300

    model flat and migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_174725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "addres"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
